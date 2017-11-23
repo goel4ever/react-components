e59deefd870f29b3196e63d9c90d17eb6fbd09ca
Anshul Goel <anshul.goel@priceline.com>
2017-11-23 00:13:30 -0500

    feat(bundle): display source for each story

--- stories/index.js
+++ stories/index.js
@@ -3,33 +3,32 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { withInfo } from '@storybook/addon-info';
+import { withInfo, setDefaults } from '@storybook/addon-info';
 
 import Header from '../src/header/Header';
 import Footer from '../src/footer/Footer';
 import Signature from '../src/signature/Signature';
 import Image from '../src/image'
 
-storiesOf('Header', module)
-  .add('Base', withInfo(`
-      description or documentation about my component, supports markdown
+setDefaults({
+  header: false,  // Toggles display of header with component name and description
+  inline: true,   // Displays info inline vs click button to view
+});
 
-      ~~~js
-      <Header />
-      ~~~
-
-    `)(() =>
-      <Header />
-    )
-  );
+storiesOf('Header')
+  .addDecorator((story, context) => withInfo('')(story)(context))
+  .add('Base', () => <Header />);
 
 storiesOf('Footer', module)
+  .addDecorator((story, context) => withInfo('')(story)(context))
   .add('Base', () => <Footer />);
 
 storiesOf('Signature', module)
+  .addDecorator((story, context) => withInfo('')(story)(context))
   .add('Base', () => <Signature />);
 
 storiesOf('Image', module)
+  .addDecorator((story, context) => withInfo('')(story)(context))
   .add('Base', () => <Image src='/78dfd6fbd480387a0610b587cbfcd0ad.jpg' />)
   .add('Grayscale', () => <Image src='/78dfd6fbd480387a0610b587cbfcd0ad.jpg' grayscale="true" />)
   .add('Blur', () => <Image src='/78dfd6fbd480387a0610b587cbfcd0ad.jpg' blur="true" />);
